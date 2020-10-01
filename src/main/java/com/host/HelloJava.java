facd7e035ad7423c1267fba21e470dc4722c6cfc
zdenko.pavlik.extern <zdenko.pavlik.extern@corpuls.com>
2020-10-01 12:22:41 +0200

    Buildable jar-with-dependencies

--- src/main/java/com/host/HelloJava.java
+++ src/main/java/com/host/HelloJava.java
@@ -1,14 +1,10 @@
-package helloworld;
+package com.host;
 
 public class HelloJava {
-
     public static void main(String args[]) {
-
         System.out.println("Hello World 1!");
         System.out.println("Hello World 2!");
         System.out.println("Hello World 3!");
-
-
     }
 
 }
